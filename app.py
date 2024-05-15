60dcd2c52367835d7173588d88c84974966b9420
Deepak Saini <deepaksainiofficial17@gmail.com>
2024-05-15 10:37:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/webhook', methods = ['GET', 'POST'])
+def webhook():
+    return "<h1>Webhook called</h1>"
+
 
 if __name__ == "__main__":
     app.run()
